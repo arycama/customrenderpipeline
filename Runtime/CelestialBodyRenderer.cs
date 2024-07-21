6d5a46e4bb807c3f2860bfb70082a83709be4ca3
Benjamin Sims <arycama@gmail.com>
2024-07-22 00:25:32 +0930

    Added celestial body renderer

---
+++ Runtime/CelestialBodyRenderer.cs
@@ -0,0 +1,24 @@
+﻿using Arycama.CustomRenderPipeline;
+using UnityEngine;
+
+public class CelestialBodyRenderer
+{
+    private RenderGraph renderGraph;
+
+    public CelestialBodyRenderer(RenderGraph renderGraph)
+    {
+        this.renderGraph = renderGraph;
+    }
+
+    public void Render(Camera camera)
+    {
+        using (var pass = renderGraph.AddRenderPass<GlobalRenderPass>("Celestial Body"))
+        {
+            var data = pass.SetRenderFunction<EmptyPassData>((command, pass, data) =>
+            {
+                foreach (var celestialBody in CelestialBody.CelestialBodies)
+                    celestialBody.Render(command, camera);
+            });
+        }
+    }
+}
