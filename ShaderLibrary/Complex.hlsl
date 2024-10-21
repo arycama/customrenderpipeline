731eac27337f73cbcb423526155c9dee8461fa0f
Benjamin Sims <arycama@gmail.com>
2024-10-22 02:54:48 +1030

    Ocean spectrum

--- ShaderLibrary/Complex.hlsl
+++ ShaderLibrary/Complex.hlsl
@@ -32,7 +32,7 @@ float2 csub(float2 c0, float2 c1)
 
 float2 cmul(float2 c0, float2 c1)
 {
-	return c0.xx * c1.xy + c0.yy * float2(-1, 1) * c1.yx;
+	return float2(c0.x * c1.x - c0.y * c1.y, c0.y * c1.x + c0.x * c1.y);
 }
 
 float2 conj(float2 c)
@@ -42,7 +42,7 @@ float2 conj(float2 c)
 
 float2 cexp(float2 c)
 {
-	return float2(cos(c.y), sin(c.y));
+	return float2(cos(c.y), sin(c.y)) * exp(c.x);
 }
 
 // https://cirosantilli.com/float2-dot-product
