87745977ad1b57601d984b70e80763383d968516
Ben Sims <arycama@gmail.com>
2026-06-03 23:42:29 +0930

    Minor tweak

--- Runtime/Utility/PhysicalLightingUtility.cs
+++ Runtime/Utility/PhysicalLightingUtility.cs
@@ -2,13 +2,23 @@ using static Unmath.Math;
 
 public static class PhysicalLightingUtility
 {
-    public static float CandelasToNits(float luminousIntensity, float area)
+    public static float CandelasToNits(float luminousIntensity, float projectedArea)
     {
-        return luminousIntensity / area;
+        return luminousIntensity / projectedArea;
     }
 
     public static float CandelasToNitsDisc(float luminousIntensity, float radius)
     {
         return CandelasToNits(luminousIntensity, Pi * Sq(radius));
+    }
+
+    public static float NitsToCandelas(float luminance, float projectedArea)
+    {
+        return luminance * projectedArea;
+    }
+
+    public static float NitsToCandelasDisc(float luminance, float radius)
+    {
+        return NitsToCandelas(luminance, Pi * Sq(radius));
     }
 }
